258ca8824ae8d7bb1531de93d377db82f15bf068
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-12-04 20:53:57 +0100

    Removed react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-const queryClient = new QueryClient();
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
