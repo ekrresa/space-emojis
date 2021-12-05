d9a8911ed6a3ad3b24aa5208ba000fd9793b8225
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-12-05 11:59:18 +0100

    pagination function

---
+++ lib/paginate.ts
@@ -0,0 +1,24 @@
+import { Emoji } from '../types';
+
+interface PaginateProps {
+  data: Emoji[];
+  currentPage: number;
+}
+
+export function paginator({ data, currentPage = 1 }: PaginateProps) {
+  let page = currentPage || 1;
+  let perPage = 100;
+  let offset = (page - 1) * perPage;
+  let paginatedItems = data.slice(offset).slice(0, perPage);
+  let totalPages = Math.ceil(data.length / perPage);
+
+  return {
+    page,
+    perPage,
+    prevPage: page - 1 ? page - 1 : null,
+    nextPage: totalPages > page ? page + 1 : null,
+    total: data.length,
+    totalPages,
+    data: paginatedItems,
+  };
+}
