26bc829883a9b1fdc3a64f4ba9977a1cbbcb6a39
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-12-06 15:18:40 +0100

    Setup testing library

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFiles: ['@testing-library/react/dont-cleanup-after-each'],
+};
+
+module.exports = createJestConfig(customJestConfig);
